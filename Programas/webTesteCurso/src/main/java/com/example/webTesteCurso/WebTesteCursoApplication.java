cc9b047194e86d3ffac91b25f019b0584742b1eb
ANTONIO ROCHA DA SILVA <antonio.silva13@alunos.sc.senac.br>
2024-10-18 14:33:33 -0300

    Adicionada pasta de programas do springboot

---
+++ Programas/webTesteCurso/src/main/java/com/example/webTesteCurso/WebTesteCursoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webTesteCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTesteCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTesteCursoApplication.class, args);
+	}
+
+}
---
+++ Programas/webTesteCurso/src/test/java/com/example/webTesteCurso/WebTesteCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webTesteCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTesteCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
